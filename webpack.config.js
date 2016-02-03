9e8c58e117611959a09994074b47cbefbbbbf57a
Yamil Llanos <yamil.llanos@gmail.com>
2016-02-03 14:21:25 -0500

    Update webpack.config.js
    Changing dir name, lowercasing main app file name. Changing file name on bundle JS file

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ var webpack = require('webpack');
  */
 var config = {
   devtool: 'eval-source-map',
-  entry:  __dirname + "/app/App.js",
+  entry:  __dirname + "/src/app.js",
   output: {
     path: __dirname + "/public",
-    filename: "bundle.js"
+    filename: "index.js"
   },
   module: {
     loaders: [{
